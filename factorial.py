733c1f9c7f81b132a9e39d85ad59a51af3257b89
SaiGaneshKadali <107615730+SaiGaneshKadali@users.noreply.github.com>
2024-04-11 11:14:12 +0530

    First commit

---
+++ factorial.py
@@ -0,0 +1,15 @@
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+def main():
+    num = int(input("Enter a number: "))
+    if num < 0:
+        print("Factorial of negative numbers is not defined.")
+    else:
+        print("Factorial of", num, "is", factorial(num))
+
+if __name__ == "__main__":
+    main()
